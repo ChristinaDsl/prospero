4cfcaa7009b25c24d5cfb1b862fd47ddbaae4fa2
ChristinaDsl <christinadsl@hotmail.com>
2024-03-01 13:52:08 +0200

    [JBEAP-26295] [installation-manager] The file URL passed as --repositories argument is not validated correctly

--- prospero-cli/src/main/java/org/wildfly/prospero/cli/RepositoryDefinition.java
+++ prospero-cli/src/main/java/org/wildfly/prospero/cli/RepositoryDefinition.java
@@ -17,14 +17,20 @@
 
 package org.wildfly.prospero.cli;
 
+import org.jboss.logging.Logger;
 import org.wildfly.channel.Repository;
 
-import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class RepositoryDefinition {
 
@@ -34,33 +40,49 @@ public class RepositoryDefinition {
             final String text = repos.get(i);
             if(text.contains("::")) {
                 final String[] parts = text.split("::");
-                if (parts.length != 2 || parts[0].isEmpty() || parts[1].isEmpty() || !isValidUrl(parts[1])) {
+                if (parts.length != 2 || parts[0].isEmpty() || parts[1].isEmpty() || !isValidUrlOrFilePath(parts[1])) {
                     throw CliMessages.MESSAGES.invalidRepositoryDefinition(text);
                 }
-
                 repositories.add(new Repository(parts[0], parts[1]));
             } else {
-                if (!isValidUrl(text)) {
+                if (!isValidUrlOrFilePath(text)) {
                     throw CliMessages.MESSAGES.invalidRepositoryDefinition(text);
                 }
-
                 repositories.add(new Repository("temp-repo-" + i, text));
             }
         }
         return repositories;
     }
 
-    private static boolean isValidUrl(String text) {
+    private static boolean isValidUrlOrFilePath(String text) {
         try {
-            URL url = new URL(text);
-            if (text.startsWith("file")){
-                String path = Paths.get(url.getPath()).normalize().toString();
-                File f = new File(path);
-                return f.exists();
+            if (text.startsWith("file://..")) {
+                Logger.getLogger("'file://../path/to/repo' is invalid as the format of file URL should be " +
+                        "'file://<HOST>/<PATH>'");
+                return false;
+            } else if (text.startsWith("file") || text.startsWith("http") ){
+                new URL(text);
+                if (text.startsWith("http")){
+                    return true;
+                }
             }
-            return true;
-        } catch (MalformedURLException e) {
+            return filePathExists(text);
+        } catch (URISyntaxException | InvalidPathException | MalformedURLException e) {
             return false;
         }
+    }
+
+    /* Pattern command, exists in order to recognize Windows file paths properly.
+       For Windows systems, Path.of(Paths.get(url.toURI()).toString()) works correctly,
+       whereas for Linux, Paths.get(url.getPath()).toAbsolutePath() is more suitable. */
+    private static boolean filePathExists(String text) throws MalformedURLException, URISyntaxException {
+        if (text.startsWith("file:")) {
+            URL url = new URL(text);
+            if (Pattern.compile("/[A-Z]:").matcher(text).find()) {
+                return Files.exists(Path.of(Paths.get(url.toURI()).toString()));
+            }
+            return Files.exists(Paths.get(url.getPath()).toAbsolutePath());
+        }
+        return Files.exists(Paths.get(new URI(text).getPath()).toAbsolutePath());
     }
 }
